fa26a8a5bb7e4e8dbe6649915de5d4f6782ef0cd
Toshiki Sasaki <s1300138@u-aizu.ac.jp>
2023-06-09 17:56:19 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class DiceGame{
     public static void main(String[] args){
+	System.out.println("What is your name?");
+
+	Scanner input = new Scanner(System.in);
+	String name = input.nextLine();
+
+	System.out.println("Hello, " + name + "!");
+
 	System.out.println("What is your name?");
 
 	Scanner input = new Scanner(System.in);
